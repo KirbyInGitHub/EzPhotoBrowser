cc4e31d91e5c8cbf122df45585cb49f626e1b0e3
zhangpeng <zhangpeng@ezbuy.com>
2017-08-17 15:22:46 +0800

    rename

--- EzPhotoBrowser/EzPhotoBrowser.h
+++ EzPhotoBrowser/EzPhotoBrowser.h
@@ -8,12 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for EzPhotoBrowse.
+//! Project version number for EzPhotoBrowser.
 FOUNDATION_EXPORT double EzPhotoBrowseVersionNumber;
 
-//! Project version string for EzPhotoBrowse.
+//! Project version string for EzPhotoBrowser.
 FOUNDATION_EXPORT const unsigned char EzPhotoBrowseVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <EzPhotoBrowse/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <EzPhotoBrowser/PublicHeader.h>
 
 
